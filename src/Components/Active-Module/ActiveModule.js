7b80ed1265c8a7e9ebec417001cd221c97fb2be0
Ashique <ashique.mohammed@justride.in>
2018-04-20 15:50:02 +0530

    Active module toggling

--- src/Components/Active-Module/ActiveModule.js
+++ src/Components/Active-Module/ActiveModule.js
@@ -5,7 +5,7 @@ export default class ActiveModule extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            // visible: props.visible || false
+            collapsed: props.collapsed || true
         }
     }
 
@@ -62,6 +62,10 @@ export default class ActiveModule extends Component {
         // this.operation(this.state.visible);
     }
 
+    toggleMenus() {
+        this.setState({ collapsed: !this.state.collapsed });
+    }
+
     render() {
         // const { visible } = this.state;
         // this.operation(visible);
@@ -69,64 +73,42 @@ export default class ActiveModule extends Component {
 
         return (
             <div className="active-module">
-                <div class="search-box">
-                    <div class="search-box-header">
-                        <div class="input-box">
-                            {/* <input placeholder="Search Menus" type="text" class="form-control mousetrap" on-change="activeModule.findMatchingMenus()"
+                <div className={`search-box ${this.state.collapsed ? '' : 'expanded'}`}>
+                    <div className="search-box-header">
+                        <div className="input-box">
+                            {/* <input placeholder="Search Menus" type="text" className="form-control mousetrap" on-change="activeModule.findMatchingMenus()"
                             ng-model="activeModule.searchText" /> */}
                         </div>
-                        <button class="btn btn-default" ng-click="activeModule.toggleMenus()">
-                            <i class="fa fa-times" aria-hidden="true"></i>
+                        <button className="btn btn-default" onClick={() => this.toggleMenus()}>
+                            <i className="fa fa-times" aria-hidden="true"></i>
                         </button>
                     </div>
-                    <div class="search-box-body">
-                        <p class="search-hint">
+                    <div className="search-box-body">
+                        <p className="search-hint">
                             Select a module to list menus in it or type to search all the menus
                     </p>
-                        <div class="panel menus">
-                            <div class="panel-heading">
+                        <div className="panel menus">
+                            <div className="panel-heading">
                                 Menus
                     </div>
-                            <div class="panel-body">
-                                {/* <div class="menu-list"
+                            <div className="panel-body">
+                                {/* <div className="menu-list"
                                 ng-click="activeModule.navigateTo(menu)" ng-repeat="menu in (filteredResults = (activeModule.selectedModule.menus|filter:activeModule.searchText))">
-                                <span class="pull-right" ng-if="activeModule.state.current.name == 'landing.'+menu.state_name">
-                                    <i class="fa fa-check" aria-hidden="true"></i>
+                                <span className="pull-right" ng-if="activeModule.state.current.name == 'landing.'+menu.state_name">
+                                    <i className="fa fa-check" aria-hidden="true"></i>
                                 </span>
                             </div> */}
 
                             </div>
                         </div>
                     </div>
-
-                    {/* <div class="panel menus" ng-if="activeModule.searchText" ng-repeat="module in activeModule.modules" ng-show="matchingFilteredResults.length">
-                    <div class="panel-heading">
-                    </div>
-                    <div class="panel-body">
-                        <div class="menu-list" ng-class="{'selected':activeModule.state.current.name=='landing.'+menu.state_name}" ng-if="(menu.active==1)&&(menu.visibility==1)"
-                            ng-click="activeModule.navigateTo(menu)" ng-repeat="menu in (matchingFilteredResults = (module.menus|filter:activeModule.searchText|filter:{active:1,visibility:1}))">
-                            <small>
-                            </small>
-                            <span class="pull-right" ng-if="activeModule.state.current.name == 'landing.'+menu.state_name">
-                                <i class="fa fa-check" aria-hidden="true"></i>
-                            </span>
-                        </div>
-
-                        <div ng-if="!matchingFilteredResults.length" class="empty">
-                            <p class="lead">
-                                No matching menus
-                            </p>
-                        </div>
-                    </div>
-                </div> */}
-
                 </div>
-                <div class="search-button">
-                    <button class="btn btn-default" ng-click="activeModule.toggleMenus()">
-                        <i class="fa fa-bars" aria-hidden="true"></i>
+                <div className="search-button">
+                    <button className="btn btn-default" onClick={() => this.toggleMenus()}>
+                        <i className="fa fa-bars" aria-hidden="true"></i>
                     </button>
                 </div>
-            </div>
+            </div >
         )
     }
 }
