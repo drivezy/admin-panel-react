7a2a9db66d83cb0e779fca086b4f7e120c83c2c2
Shubham <shubham.kesarwani@drivezy.com>
2018-04-20 17:58:07 +0530

    Data preparation for generic listing done

--- src/Scenes/Generic-Listing/genericListing.scene.js
+++ src/Scenes/Generic-Listing/genericListing.scene.js
@@ -2,19 +2,55 @@ import React, { Component } from 'react';
 import './genericListing.css';
 import { GetUrlParams } from './../../Utils/location.utils';
 import { GetMenuDetail } from './../../Utils/generic.utils';
-import { ConvertMenuDetailForGenericListing } from './../../Utils/genericListing.utils';
+import { ConvertMenuDetailForGenericListing, GetListingRecord } from './../../Utils/genericListing.utils';
 
 export default class GenericListing extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            ...GetUrlParams(this.props)
-        }
+            ...GetUrlParams(this.props), // params, queryString
+            menuDetail: {},
+            genericData: {}
+        };
         console.log(this.state);
     }
 
     componentDidMount() {
+        this.getMenuData();
+    }
 
+    getMenuData = async () => {
+        const { queryString } = this.state;
+        const { menu_id } = queryString;
+        const result = await GetMenuDetail(menu_id);
+        if (result.success) {
+            console.log(result.response);
+            const { response = {} } = result;
+            const menuDetail = ConvertMenuDetailForGenericListing(response || {});
+            if (typeof response.controller_path == 'string' && response.controller_path.includes('genericListingController.js') != -1) {
+                this.setState({ menuDetail });
+                this.getListingData();
+            }
+        }
+    }
+
+    getListingData = () => {
+        const { menuDetail, genericData, urlParameter } = this.state;
+        GetListingRecord({ configuration: menuDetail, callback: this.dataFetched, data: genericData, urlParameter });
+    }
+
+    dataFetched = (genericData) => {
+        // const totalPages = Math.ceil((genericData.stats.records / genericData.stats.count));
+
+        // if (totalPages > 7) {
+        //     // this.setState({ pagesOnDisplay: 7 });
+        //     this.state.pagesOnDisplay = 7;
+        // } else {
+        //     // this.setState({ pagesOnDisplay: totalPages });
+        //     this.state.pagesOnDisplay = Math.ceil(totalPages);
+        // }
+        console.log('genericData', genericData);
+        this.setState({ genericData });
     }
 
     render() {
--- src/Utils/generic.utils.js
+++ src/Utils/generic.utils.js
@@ -1,4 +1,6 @@
 import { Get } from './http.utils';
+import { IsUndefinedOrNull } from './common.utils';
+
 import { GetMenuDetailEndPoint } from './../Constants/api.constants';
 
 /**
@@ -9,4 +11,77 @@ import { GetMenuDetailEndPoint } from './../Constants/api.constants';
 export function GetMenuDetail(menuId, callback) {
     const url = GetMenuDetailEndPoint + menuId;
     return Get({ url, callback, persist: callback ? true : false });
+}
+
+/**
+ * takes query as string & evaluates them
+ * replace string variable to their value
+ * @param  {string} params
+ * @returns evaluated query
+ */
+export function ConvertToQuery(params) {
+    const reg = /(:[$\w.]*)\w+/g;
+    const tempArr = params.match(reg);
+
+    for (const i in tempArr) {
+        if (tempArr[i] && typeof tempArr[i] == 'string') {
+            const a = eval(tempArr[i].split(':')[1]);
+            const b = tempArr[i];
+            params = params.replace(b, a);
+        }
+    }
+    return params;
+}
+
+/**
+ * takes dictionary and relationship and create object having key combination of its parent and id
+ * used for getting list of columns in above explained format which is again used by CreateFinalColumns method to return selected columns
+ * @param  {string} {includes
+ * @param  {object} relationship
+ * @param  {string} starter
+ * @param  {object} dictionary
+ * @param  {boolean} excludeStarter}
+ */
+export function GetColumnsForListing({ includes, relationship, starter, dictionary, excludeStarter }) {
+    const columns = [];
+    const selectedColumns = {};
+    const includesList = [];
+    const includesArr = includes.split(',');
+
+    // for (const i in includesArr) {
+    for (let k = 0; k < includesArr.length; k++) {
+        const tempIncludes = includesArr[k].split('.');
+        let newStarter = starter;
+        for (let j = 0; j < tempIncludes; j++) {
+            newStarter += `.${tempIncludes[j]}`;
+            includesList.push(newStarter);
+        }
+    }
+
+    !excludeStarter ? includesList.unshift(starter) : null;
+    for (let k = 0; k < includesList.length; k++) {
+        columns[includesList[k]] = dictionary[(includesList[k])];
+    }
+    // columns = dictionary;
+    for (const i in columns) {
+        // const data = columns[i];
+        for (let j = 0; j < columns[i].length; j++) {
+            const element = `${i}.${columns[i][j].column_name}`;
+
+            columns[i][j].path = element.replace(/\.?([A-Z]+)/g, (x, y) => {
+                return `_${y.toLowerCase()}`;
+            }).replace(/^_/, '').replace(starter, '').replace('.', '');
+            columns[i][j].parent = i;
+
+            const relationIndex = columns[i][j].parent;
+            if (!IsUndefinedOrNull(relationship) && relationship.hasOwnProperty(relationIndex) && relationship[relationIndex].hasOwnProperty('related_model')) {
+                columns[i][j].reference_route = relationship[relationIndex].related_model.state_name;
+                columns[i][j].parentColumn = relationship[relationIndex].related_column ? relationship[relationIndex].related_column.column_name : null;
+            }
+
+            selectedColumns[`${columns[i][j].parent}.${columns[i][j].id}`] = columns[i][j];
+            // selectedColumns[columns[i][j].id] = columns[i][j];
+        }
+    }
+    return selectedColumns;
 }
