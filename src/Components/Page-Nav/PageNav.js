25c8b4b246735de4ef41e7980d062870c9d09c8c
Dheeraj Prajapati <dheeraj.prajapati@justride.in>
2018-05-02 16:10:06 +0530

    Added signout

--- src/Components/Page-Nav/PageNav.js
+++ src/Components/Page-Nav/PageNav.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './PageNav.css';
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
+import GLOBAL from './../../Constants/global.constants';
+
+import { Get } from './../../Utils/http.utils';
+
+import {
+    Redirect
+} from 'react-router-dom';
 
 export default class PageNav extends Component {
 
@@ -20,7 +27,36 @@ export default class PageNav extends Component {
         });
     }
 
+    logout = async () => {
+        const res = await Get({ urlPrefix: GLOBAL.ROUTE_URL, url: 'logout' });
+        // const l = res;
+        if (res.success) {
+
+            const a = (this.props.location ? this.props.location.state : null) || { from: { pathname: '/login' } };
+
+            // this.props.history.push("/login");
+            // location.href
+            // alert('user loggedout successfully');
+
+            this.setState({ redirectToReferrer: true });
+        }
+    }
+
     render() {
+        const { from } = (this.props.location ? this.props.location.state : null) || { from: { pathname: '/login' } };
+        const { redirectToReferrer } = this.state
+        // this.props.setCurrentRoute(from)
+        if (redirectToReferrer) {
+            // Global.currentRoute = from;
+            return (
+                <Redirect to={from} />
+            )
+        }
+        // const from = { pathname: '/' };
+        // // this.logout();
+        // return (
+        //     <Redirect to={from} />
+        // )
         return (
             <div className="page-nav">
                 <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
@@ -38,9 +74,9 @@ export default class PageNav extends Component {
                         <DropdownItem>Change Password</DropdownItem>
                         <DropdownItem>Configure Search</DropdownItem>
                         <DropdownItem>Impersonate User</DropdownItem>
-                        <DropdownItem>Sign Out</DropdownItem>
+                        <DropdownItem onClick={(event) => { event.preventDefault(); this.logout() }}>Sign Out</DropdownItem>
                     </DropdownMenu>
-                    
+
                 </ButtonDropdown>
             </div>
         );
