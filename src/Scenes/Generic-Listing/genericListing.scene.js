8b3b123a26a942f642414c99ba790a824f70617e
Ashique <ashique.mohammed@justride.in>
2018-05-03 17:37:15 +0530

    Style changes on the table

--- src/Scenes/Generic-Listing/genericListing.scene.js
+++ src/Scenes/Generic-Listing/genericListing.scene.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
-import { Table } from 'reactstrap';
+import {
+    Table, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle,
+} from 'reactstrap';
 
 import './genericListing.css';
 import { GetUrlParams } from './../../Utils/location.utils';
@@ -62,33 +65,95 @@ export default class GenericListing extends Component {
 
         return (
             <div className="generic-listing-container">
-
-                <Table striped>
-                    <thead>
-                        <tr>
-                            {
-                                finalColumns.map((selectedColumn, key) => (
-                                    <th key={key}> {selectedColumn.display_name}</th>
-                                ))
-                            }
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            listing.map((listingRow, rowKey) => (
-                                <tr key={rowKey}>
+                <Card>
+                    <CardBody>
+                        <Table striped>
+                            <thead>
+                                <tr>
+                                    <th>
+                                    </th>
                                     {
                                         finalColumns.map((selectedColumn, key) => (
-                                            <td key={key}>
-                                                {eval('listingRow.' + selectedColumn.path)}
-                                            </td>
+                                            <th className="column-header" key={key}>
+                                                {/* Column Wrapper */}
+                                                <div className="column-wrapper">
+                                                    {/* Column Title */}
+                                                    <div className="column-title printable">
+                                                        {selectedColumn.display_name}
+                                                        {/* {{ b.columnTitle || selectedColumn.display_name }} */}
+                                                    </div>
+                                                    {/* Column Title Ends */}
+
+                                                    {/* Filter Column */}
+                                                    {
+                                                        selectedColumn.path.split('.').length < 3 &&
+                                                        <div className="filter-column">
+                                                            <a ng-click="portlet.preventDefault($event);portlet.filterColumn(select-edColumn)">
+                                                                <i className="fas fa-filter"></i>
+                                                            </a>
+                                                        </div>
+                                                    }
+                                                    {/* Filter Ends */}
+                                                    {/* DB Level */}
+
+                                                    {
+                                                        (selectedColumn.path.split('.').length == 1) && (selectedColumn.column_type != 118) &&
+                                                        (
+                                                            <div className="db-level-sort">
+                                                                <span>
+                                                                    <a className="dropdown-link" id="simple-dropdown">
+                                                                        <i className="fa fa-sort-amount-asc"></i>
+                                                                    </a>
+
+                                                                </span>
+                                                            </div>
+                                                        )
+                                                    }
+                                                    {/* <div class="db-level-sort" ng-if="selectedColumn.path.split('.').length==1&&selectedColumn.column_type!=118">
+                                                        <span uib-dropdown on-toggle="toggled(open)">
+                                                            <a class="dropdown-link" id="simple-dropdown" uib-dropdown-toggle>
+                                                                <i class="fa fa-sort-amount-asc"></i>
+                                                            </a>
+                                                            <ul class="dropdown-menu" uib-dropdown-menu aria-labelledby="simple-dropdown">
+                                                                <li ng-repeat="sort in portlet.sortTypes" ng-click="portlet.sortOnDB(sort, selectedColumn.path)">
+                                                                    <a>
+                                                                        <i class="fa {{sort.icon}}"></i>
+                                                                        {{ sort.caption }}
+                                                                    </a>
+                                                                </li>
+                                                            </ul>
+                                                        </span>
+                                                    </div> */}
+                                                    {/* DB Level Ends */}
+                                                </div>
+                                            </th>
                                         ))
                                     }
                                 </tr>
-                            ))
-                        }
-                    </tbody>
-                </Table>
+                            </thead>
+                            <tbody>
+                                {
+                                    listing.map((listingRow, rowKey) => (
+                                        <tr className="table-row" key={rowKey}>
+
+                                            <td>
+                                                {rowKey + 1}
+                                            </td>
+
+                                            {
+                                                finalColumns.map((selectedColumn, key) => (
+                                                    <td key={key}>
+                                                        {eval('listingRow.' + selectedColumn.path)}
+                                                    </td>
+                                                ))
+                                            }
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </Table>
+                    </CardBody>
+                </Card>
             </div>
         );
     }
