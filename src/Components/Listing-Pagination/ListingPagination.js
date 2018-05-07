4e5c5d70de62324b1599f9aef974358cf125e1b9
Dheeraj Prajapati <dheeraj.prajapati@justride.in>
2018-05-07 14:46:22 +0530

    updates listing pagination

--- src/Components/Listing-Pagination/ListingPagination.js
+++ src/Components/Listing-Pagination/ListingPagination.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './ListingPagination';
+import './ListingPagination.css';
 
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
@@ -19,10 +19,14 @@ export default class ListingPagination extends Component {
         let previousPage;
         let nextPage;
         const pages = [];
+        const showPages = 5;
+        let showFlag = '.....';
+
         if (genericData.stats) {
             var number_of_pages = Math.round(genericData.stats.records / genericData.stats.count);
             console.log(number_of_pages);
-            for (let i = 1; i < number_of_pages; i++) {
+
+            for (let i = 1; i <= showPages; i++) {
                 pages.push({ page: i });
             }
         }
@@ -31,8 +35,6 @@ export default class ListingPagination extends Component {
             previousPage = parseInt(genericData.currentPage) - 1;
             nextPage = parseInt(genericData.currentPage) + 1;
         }
-
-
 
         return (
             <div className="listing-pagination">
@@ -43,7 +45,7 @@ export default class ListingPagination extends Component {
                     {
                         pages && pages.length && pages.map((key, count) => {
                             return (
-                                <PaginationItem key={count}>
+                                <PaginationItem className={genericData.currentPage == key.page ? 'highlighted-page' : ''} key={count}>
                                     <PaginationLink href={`?query=limit=20&page=${key.page}`} >
                                         {key.page}
                                     </PaginationLink>
@@ -51,7 +53,23 @@ export default class ListingPagination extends Component {
                             )
                         })
                     }
-                    <PaginationItem disabled={nextPage == number_of_pages+1}>
+
+                    {
+                        number_of_pages > showPages ?
+                            <PaginationItem>
+                                <PaginationLink href={`?query=limit=20&page=${nextPage}`}>
+                                    {genericData.currentPage} {showFlag}
+                                </PaginationLink>
+                            </PaginationItem>
+                            :
+                            null
+                    }
+                    <PaginationItem>
+                        <PaginationLink href={`?query=limit=20&page=${number_of_pages}`} >
+                            {number_of_pages}
+                        </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem disabled={nextPage == number_of_pages + 1}>
                         <PaginationLink next href={`?query=limit=20&page=${nextPage}`} />
                     </PaginationItem>
                 </Pagination>
