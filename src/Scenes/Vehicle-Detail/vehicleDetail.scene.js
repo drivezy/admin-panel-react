668400cba9db03648079f1907f500d501c94caa3
Dheeraj Prajapati <dheeraj.prajapati@justride.in>
2018-05-11 10:17:01 +0530

    Removed commented code

--- src/Scenes/Vehicle-Detail/vehicleDetail.scene.js
+++ src/Scenes/Vehicle-Detail/vehicleDetail.scene.js
@@ -3,7 +3,6 @@ import {
     Card, CardHeader, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button, Row, Col, Progress
 } from 'reactstrap';
-// import { TabContent, TabPane, Nav, NavItem, NavLink, Card, CardBody, Button, CardTitle, CardText, Row, Col, Progress } from 'reactstrap';
 import classnames from 'classnames';
 
 import GLOBAL from './../../Constants/global.constants';
@@ -33,7 +32,6 @@ export default class VehicleDetail extends Component {
 
         if (result.success) {
             const vehicleDetail = result.response;
-            console.log(vehicleDetail);
             this.setState({ vehicleDetail });
         }
     }
@@ -99,85 +97,6 @@ export default class VehicleDetail extends Component {
                     </Card>
                 }
             </div>
-
-
-
-
-
-            // <div className="vehicle-detail">
-            //     {
-            //         vehicleDetail.id &&
-            //         <Card className="booking-panel-container">
-            //             <CardHeader>
-            //                 <span>All Vehicle Detail | {vehicleDetail.car.name} | {vehicleDetail.registration_number}</span>
-            //             </CardHeader>
-            //             <Row className="booking-panels">
-            //                 <Col sm="4" className="no-padding">
-            //                     <div className="booking-details">
-            //                         <CardBody>
-            //                             <img className="media-object width-100" src={`${vehicleDetail.car.image}`} alt="Car Image" />
-            //                             <i className="fa fa-home roboto-medium opacity-8" aria-hidden="true"></i>
-            //                             <span className="roboto-medium opacity-8">{vehicleDetail.venue.name}</span>
-            //                         </CardBody>
-            //                     </div>
-            //                 </Col>
-            //                 <Col sm="8" className="no-padding">
-            //                     <CardBody>
-            //                         <Row>
-            //                             <Col sm="6">
-            //                                 Caution Amount
-            //                                 </Col>
-            //                             <Col sm="6">
-            //                                 {vehicleDetail.car.caution_amount}
-            //                             </Col>
-            //                         </Row>
-            //                         <Row>
-            //                             <Col sm="6">
-            //                                 Min Booking Duration
-            //                                 </Col>
-            //                             <Col sm="6">
-            //                                 {vehicleDetail.car.min_period / 60}
-            //                             </Col>
-            //                         </Row>
-            //                         <Row>
-            //                             <Col sm="6">
-            //                                 Fuel Eficiency
-            //                                 </Col>
-            //                             <Col sm="6">
-            //                                 {vehicleDetail.car.fuel_efficiency}
-            //                             </Col>
-            //                         </Row>
-            //                         <Row>
-            //                             <Col sm="6">
-            //                                 Extra Km Charges
-            //                                 </Col>
-            //                             <Col sm="6">
-            //                                 {vehicleDetail.car.extra_km_charges}
-            //                             </Col>
-            //                         </Row>
-            //                         <Row>
-            //                             <Col sm="6">
-            //                                 Free Km Per Hour
-            //                                 </Col>
-            //                             <Col sm="6">
-            //                                 {vehicleDetail.car.distance}
-            //                             </Col>
-            //                         </Row>
-            //                         {vehicleDetail.vehicle_detail.speed_limit &&
-            //                             <Row>
-            //                                 <Col sm="6">
-            //                                     Speed Limit
-            //                                 </Col>
-            //                                 <Col sm="6">
-            //                                     {vehicleDetail.vehicle_detail.speed_limit}
-            //                                 </Col>
-            //                             </Row>}
-            //                     </CardBody>
-            //                 </Col>
-            //             </Row>
-            //         </Card>
-            //     }
-            // </div>
         )
     }
 }
