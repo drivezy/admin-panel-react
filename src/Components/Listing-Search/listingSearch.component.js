ffe11e948a2650257104e996d08d9f9c7b1dfe4b
Shubham <shubham.kesarwani@drivezy.com>
2018-05-25 19:55:13 +0530

    Search on generic listing

--- src/Components/Listing-Search/listingSearch.component.js
+++ src/Components/Listing-Search/listingSearch.component.js
@@ -4,7 +4,6 @@ import { Collapse, Button, CardBody, Card } from 'reactstrap';
 import SelectBox from './../Forms/Components/Select-Box/selectBox';
 
 import { SubscribeToEvent, UnsubscribeEvent } from './../../Utils/stateManager.utils';
-//import { SelectFromOptions, BuildUrlForGetCall, IsUndefinedOrNull, IsUndefined } from './../../Utils/common.utils';
 
 import './listingSearch.component.css'
 import GLOBAL from './../../Constants/global.constants';
@@ -20,40 +19,33 @@ import DatePicker from './../Forms/Components/Date-Picker/datePicker';
 let activeColumn = {};
 
 export default class ListingSearch extends React.Component {
-
     urlParams = Location.search();
-
     constructor(props) {
         super(props);
-
         this.state = {
-            isCollapsed: true,
-            filterArr: [],
-            query: '',
-            sort: null,
-            order: null,
-            activeFilter: {},
-            scopes: [],
             selectedColumn: {},
             getObj: {},
             inputValue: ''
         };
     }
 
-    /**
-     * Resets all columns back to only one blank query
-     */
-    resetColumns = () => {
-        this.state.filterArr = [
-            [{ ...this.filterObj }]
-        ];
-        // this.setState({ filterArr: [this.filterObj] });
+    componentDidMount() {
+        this.initialize();
     }
 
-    callFilterChangeAfterColumnChange(filterArr, { parentIndex, childIndex }) {
-        filterArr[parentIndex][childIndex].filter = filterArr[parentIndex][childIndex].filterField[0];
-        this.setState({ filterArr });
-        this.filterChange(filterArr[parentIndex][childIndex].filter, { parentIndex, childIndex, dontPropagateFocus: true });
+    componentWillReceiveProps() {
+        this.initialize();
+    }
+
+    // Initialize the controller
+    initialize() {
+        const { dictionary, searchQuery } = this.props;
+
+        if (searchQuery) {
+            const values = searchQuery.split(' ');
+            console.log(values);
+        }
+        // SelectFromOptions()
     }
 
     /**
@@ -65,8 +57,6 @@ export default class ListingSearch extends React.Component {
      */
     filterChange(select) {
         let valueColumnType = "input";
-        let { filterArr } = this.state;
-
         this.setState({
             selectedColumn: select
         })
@@ -88,7 +78,8 @@ export default class ListingSearch extends React.Component {
             const queryField = queryFieldName ? queryFieldName : displayName;
             let url = activeColumn.referenced_model.route_name;
             const options = {
-                query: queryField + ' like %22%25' + val + '%25%22'
+                query: queryField + ' like "%' + val + '%"'
+                // query: queryField + ' like %22%25' + val + '%25%22'
             };
 
             if (activeColumn.sorting_type) {
@@ -115,52 +106,20 @@ export default class ListingSearch extends React.Component {
             getObj: data
         })
 
-        const paramProps = {
-            history: this.props.history, match: this.props.match
-        };
         switch (activeColumn.column_type) {
             case 116: case 117:
-                query += activeColumn.column_name + '=' + data.data.id;
+                query += activeColumn.column_name + ' = ' + data.data.id;
                 break;
 
             default:
-                query += activeColumn.column_name + '=' + data.data;
+                query += activeColumn.column_name + ' = ' + data.data;
                 break;
         }
 
         const urlParams = this.urlParams;
         urlParams.search = query;
-        Location.search(urlParams, { props: paramProps });
-
+        Location.search(urlParams);
     };
-
-    // Initialize the controller
-    initialize() {
-        //const { content } = this.props;
-
-        // filterObj is a basic data structure that is defined and used
-        // across this controller
-        // @todo Should rewrite this part
-        this.filterObj = {
-            selectField: {},
-            filterField: this.filterType,
-            column: {},
-            filter: {},
-            inputField: null,
-            selectValue: {},
-            slot: { startDate: null, endDate: null },
-        };
-    }
-
-    activeFilter = (index) => {
-        this.state.activeFilter = {};
-        const { filters = [] } = this.props;
-        filters.forEach((filter, key) => {
-            if (index == filter.id) {
-                this.setState({ activeFilter: filter });
-            }
-        });
-    }
 
     callFunction = () => {
         let query = '';
@@ -170,7 +129,7 @@ export default class ListingSearch extends React.Component {
             history: this.props.history, match: this.props.match
         };
 
-        query += activeColumn.column_name + ' like' + this.state.inputValue;
+        query += activeColumn.column_name + ' like "%' + this.state.inputValue + '%"';
         urlParams.search = query;
         Location.search(urlParams, { props: paramProps });
 
@@ -181,7 +140,6 @@ export default class ListingSearch extends React.Component {
             this.callFunction();
             return;
         }
-
         this.setState({ inputValue: this.state.inputValue + e.key })
     }
 
