69ed1de752426b342dbeb595ac74fca5450f698f
Ashique <ashique.mohammed@justride.in>
2018-06-06 19:19:42 +0530

    Import bootstrap and theme changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Landing from './Components/Landing/landing.component';
 
