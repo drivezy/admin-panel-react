e38274716e78d294295bcffc7519376c00be7bcb
Ashique <ashique.mohammed@justride.in>
2018-06-18 16:34:37 +0530

    Form Setting Preference fix

--- src/Constants/global.constants.js
+++ src/Constants/global.constants.js
@@ -12,6 +12,12 @@ module.exports = {
         TEXT_COLOR: '#333',
         PRIMARY_COLOR: '#31b6e7'
     },
+    ORGANIZATION: {
+        name: 'Drivezy',
+        logo: '//s3.ap-south-1.amazonaws.com/drivezy.com-assets/drivezy-brand-logo.png',
+        headerLogo: 'https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1524554137_logo-main.png',
+        secure: true
+    },
     DATE_TIME_FORMAT: 'YYYY-MM-DD HH:mm',
     API_DATE_TIME_FORMAT: 'YYYY-MM-DD HH:mm:ss',
     DISPLAY_DATE_TIME_FORMAT: 'DD MMM YYYY, HH:mm',
