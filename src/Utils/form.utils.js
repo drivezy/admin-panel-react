f5aa07d391307a0b59fd1cb1c73f3663e677ffdc
Shubham Kesarwani <shubham.kesarwani89@gmail.com>
2018-06-21 10:28:44 +0530

    form util documentation and change in structure

--- src/Utils/form.utils.js
+++ src/Utils/form.utils.js
@@ -28,33 +28,32 @@ export default class FormUtil {
         self.form.data[column.name] = value;
         const callback = onChangeListeners[column.path];
         if (typeof callback == 'function') {
-            callback(event, column);
+            callback(value, column, event);
         }
     }
 
-    /**
-     * if id is provided return particular element else returns form element
-     * @param  {string} column {optional}
-     */
-    static getElement(column) {
-        if (!column) {
-            return document.getElementsByTagName("form")[0];
-        }
-        return document.getElementById(column);
-    };
+    // /**
+    //  * if id is provided return particular element else returns form element
+    //  * @param  {string} column {optional}
+    //  */
+    // static getElement(column) {
+    //     if (!column) {
+    //         return document.getElementsByTagName("form")[0];
+    //     }
+    //     return document.getElementById(column);
+    // };
 
     /**
      * Used to change page name
      * @param  {string} name
-     * @param  {object} form
      */
-    static pageName(name, form) {
+    static pageName(name) {
         self.form.name = name;
     }
 
     /**
-     * set element to visible or invisible depending on visibility
-     * @param  {string} column
+     * set column field to visible or invisible depending on visibility
+     * @param  {string} column - column name
      * @param  {boolean} visibility
      */
     static setVisible(column, visibility) {
@@ -63,9 +62,8 @@ export default class FormUtil {
 
 
     /**
-     * returns ngModel value of the column type param
-     * @param  {object} form - form object
-     * @param  {string} column - column type
+     * returns value of the provided column
+     * @param  {string} column - column name
      */
     static getValue(column) {
         if (typeof self.form == "object" && self.form.data[column]) {
@@ -76,9 +74,8 @@ export default class FormUtil {
     };
 
     /**
-     * sets value of input fields & textareas
-     * @param  {object} form - form ngmodel value
-     * @param  {string} column - column type
+     * sets value of input fields
+     * @param  {string} column - column name
      * @param  {any} value - value to be set for particular element
      */
     static setValue(column, value) {
@@ -91,20 +88,18 @@ export default class FormUtil {
     };
 
     /**
-     * sets value of input fields & textareas
-     * @param  {object} form - form ngmodel value
-     * @param  {string} column - column type
-     * @param  {any} value - value to be set for particular element
+     * returns dictionary value for provided column
+     * @param  {string} column - column name
      */
     static getAttribute(column) {
         return self.form.dictionary[column];
     };
 
     /**
-     * sets value of input fields & textareas
-     * @param  {object} form - form ngmodel value
-     * @param  {string} column - column type
-     * @param  {any} value - value to be set for particular element
+     * sets dictionary value for provided column
+     * for e.g. form.setAttribute('description', {display_name: 'Desc'}) - modified 'description' column's displayname to 'Desc'
+     * @param  {string} column - column name
+     * @param  {object} attribute - key value 
      */
     static setAttribute(column, attribute) {
         const dict = { ...self.form.dictionary[column], ...attribute };
@@ -116,7 +111,8 @@ export default class FormUtil {
 
     /**
      * disables particular form element
-     * @param  {string} column - column type
+     * for e.g. form.setDisabled('description', true) - make description field disabled
+     * @param  {string} column - column name
      * @param  {boolean} value -true if disabled, false if enabled
      */
     static setDisabled(column, value) {
@@ -125,73 +121,15 @@ export default class FormUtil {
     }
 
     /**
-     * adds extra attribute to element and make field required
-     * @param  {string} column
+     * makes field required
+     * for e.g. form.setMandatory('description' true) - make description field mandatory
+     * @param  {string} column - column name
      * @param  {boolean} value -true if disabled, false if enabled
      */
     static setMandatory(column, value) {
         self.form.dictionary[column].required = value;
         FormUtil.updateForm(false);
     }
-
-    /**
-     * registers method against given form element, triggers callback on change of form elemenets
-     * @param  {object} callBack - callback method to be triggered on change
-     * @param  {string} column {optional} - returns data of only particular column
-     * @param  {object} formElement {optional} - element of form to be watched
-     */
-    // static OnChange(column, callBack, formElement) {
-    //     const formUtil = new FormUtil();
-    //     if (!formElement) {
-    //         formElement = formUtil.GetElement();
-    //     }
-
-    //     formElement.addEventListener("change", function (event) {
-    //         const scopeObj = formUtil.GetScopeValue(event, formElement, column); // returns latest value
-    //         const oldVal = this.getSelectedColumn(self.form.data, column);
-
-    //         // @TODO right now callback is triggered for change in any element, make callback to trigger only for
-    //         // change in particular element
-    //         callBack(scopeObj, oldVal);
-    //     }, true);
-    // };
-
-    /**
-     * manually triggers change event for ui-slect field
-     * @param  {object} detail - object is attached to send along with event object
-     */
-    static GetSelectOnChangeUpdate(detail) {
-        const formUtil = new FormUtil();
-        const formElement = formUtil.GetElement();
-        if ("createEvent" in document) {
-            const evObj = document.createEvent("HTMLEvents");
-
-            evObj.detail = detail;
-            evObj.initEvent("change", true, true);
-            formElement.dispatchEvent(evObj);
-        } else {
-            formElement.fireEvent("onchange");
-        }
-    };
-
-    /**
-     * saves form without closing modal
-     */
-    static save() {
-        const formUtil = new FormUtil();
-        const formELement = formUtil.GetElement();
-        // angular.element(formELement).scope().$parent.modalObj.submitForm(true);
-    };
-
-    /**
-     * saves form and close modal
-     * use this method only with onChange
-     */
-    static submit() {
-        const formUtil = new FormUtil();
-        // const formElement = formUtil.GetElement();
-        // angular.element(formELement).scope().$parent.modalObj.submitForm(false);
-    };
 
     /**
      * every time new script is to be run, this method is invoked to update form object value
@@ -202,53 +140,29 @@ export default class FormUtil {
     };
 
     /**
-     * Once script is executed, returns modified form obj
+     * Returns form object
+     * After script execution, getForm is invoked to fetch modified form value
      * @param  {boolean} clearFormValue
      */
-    static getForm(clearFormValue) {
+    static getForm() {
         const form = self.form;
-        // self.form = {};
-        // self.onChangeListeners = {};
         return form;
     }
 
     /**
-     * used to manipulate elements
-     * it first checks if element is ui-select or normal input or textarea and then acts acrrodingly
-     * @param  {string} column
-     * @param  {array} keyVal - array of objects containing what action to be taken if element is ui-select of
-     *     normal input
+     * Clears scope
      */
-    manupulateUI(column, keyVal) {
-        const formUtil = new FormUtil();
-        const element = formUtil.GetElement(column);
-        let input = element.querySelector("select-field");
-        // const input = angular.element(element.querySelector("select-field"));
-        if (IsObjectHaveKeys(input)) { // if ui-select field
-            input.attr(keyVal[0].key, keyVal[0].value);
-        } else if (input = (element.querySelector("input") || element.querySelector("input"))) { // if input field or textarea
-            // } else if (input = (angular.element(element.querySelector("input")) || angular.element(element.querySelector("input")))) { // if input field or textarea
-            input.attr(keyVal[1].key, keyVal[1].value);
-        }
-        this.compileHTML(input);
+    static clearFormObject() {
+        self.form = {};
+        onChangeListeners = {};
     }
-
-
-
 
     /**
-     * when there is new attribute added, it needs to be compiled
-     * @param  {} input
+     * Makes modified form object available in formCreator class to appear changes on the ui
+     * NOTE : It is suggested to avoid using this method as after execution of script, updateForm is invoked implicitly
+     * @param  {boolean} updateState=true - updateState is used to check if DOM should also be refreshed along with form object
+     * default value is true
      */
-    compileHTML(input) {
-        // const $scope = input.scope();
-        // const $injector = input.injector();
-        // $injector.invoke(function ($compile) {
-        //     $compile(input)($scope);
-        // });
-    }
-
-
     static updateForm(updateState = true) {
         StoreEvent({ eventName: 'formChanged', data: { ...self.form, ...{ updateState } } });
     }
