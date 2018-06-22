279b5c0397f0a015afffd7b1709c17145885bcc7
ajit kumar pandey <ajit.pandey@drivezy.com>
2018-06-22 12:59:53 +0530

    modified custom tooltip where replace reactrstrap tooltip from tooltip library

--- src/Components/Custom-Tooltip/customTooltip.component.js
+++ src/Components/Custom-Tooltip/customTooltip.component.js
@@ -1,34 +1,27 @@
-import React, { Component } from 'react';
-import { Button, Row, Col, Tooltip } from 'reactstrap';
 
+  /**
+   *  modified Component which replace reactstrap tooltip from tooltip library
+   */
+import React, { Component } from 'react';
+/**
+ * import library:
+ * Tooltip: showing some content during hover of any custom tool component
+ */
+import Tooltip from '@atlaskit/tooltip';
 export default class CustomTooltip extends Component {
     constructor(props) {
-        const generateID = Math.random().toString(36).substr(2, 9);
         super(props);
-        this.state = {
-            tooltipOpen: false,
-            id: `tooltip-${generateID}`
-        };
-    }
-    toggle = () => {
-        this.setState({
-            tooltipOpen: !this.state.tooltipOpen,
-        });
-    }
-
-    render() {
+   }
+   render() {
         const { html, title, placement } = this.props;
         return (
-            <span id={this.state.id} className="tooltip-element">
-                {html}
-                <Tooltip placement={placement} isOpen={this.state.tooltipOpen} target={this.state.id} toggle={this.toggle}>
-                    <Row>
-                        <Col>
-                            <span>{title}</span>
-                        </Col>
-                    </Row>
-                </Tooltip>
-            </span>
+            /* showing content in custom tool component */
+            <Tooltip position={placement} content={title} >
+                <span className="tooltip-element">
+                    {html}
+                </span>
+            </Tooltip>
         )
     }
 }
+
