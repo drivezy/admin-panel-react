4018ca4c1df3d6502358e61516e3e8f3232af393
Mangesh Lokhande <mangesh.lokhande@drivezy.com>
2018-07-11 12:30:41 +0530

    Saving Some Changes

--- src/Components/Configure-Settings/configureSettings.component.js
+++ src/Components/Configure-Settings/configureSettings.component.js
@@ -24,7 +24,10 @@ const SpotlightTab = () =>
                 <CardText>
                     Spotlight helps you quickly access different menus in Panel . You can also search cars, vehicles, booking
                     , cities and quickly navigate to the page by pressing enter
-                                        </CardText>
+                </CardText>
+                {
+                    this.keyboardListener()
+                }
             </Card>
 
         </Col>
@@ -86,96 +89,108 @@ export default class ConfugreSettings extends Component {
         }
     }
 
-    render() {
-        const { activeTab, selectedTheme, selectedSpacing } = this.state;
-        return (
-            <div className="configure-settings">
-                <ModalBody >
-                    <Nav tabs>
-                        <NavItem>
-                            <NavLink className={`${activeTab == 1 ? 'active' : ''}`} onClick={() => { this.toggle('1'); }}>
-                                Themes
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className={`${activeTab == 2 ? 'active' : ''}`} onClick={() => { this.toggle('2'); }}>
-                                Spotlight
-                            </NavLink>
-                        </NavItem>
-                    </Nav>
-                    <TabContent activeTab={this.state.activeTab} onKeyPress={() => { console.log('pressed') }}>
-                        <TabPane tabId="1" >
-                            <Row >
-                                <Col sm="6">
-                                    <Card body className="tab-card">
-                                        <CardTitle>Choose Theme</CardTitle>
-                                        <CardText>
-                                            Select one of the below themes for your panel.
+    keyboardListener = (event) => {
+        
+            event.preventDefault();
+            setTimeout(function () {
+                if (this.state.keys.length < 2) {
+                    this.state.keys.push({
+                        key: event.key, keyCode: event.keyCode
+                    });
+                }
+            });
+        }
 
-
+        render() {
+            const { activeTab, selectedTheme, selectedSpacing } = this.state;
+            return (
+                <div className="configure-settings">
+                    <ModalBody >
+                        <Nav tabs>
+                            <NavItem>
+                                <NavLink className={`${activeTab == 1 ? 'active' : ''}`} onClick={() => { this.toggle('1'); }}>
+                                    Themes
+                            </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className={`${activeTab == 2 ? 'active' : ''}`} onClick={() => { this.toggle('2'); }}>
+                                    Spotlight
+                            </NavLink>
+                            </NavItem>
+                        </Nav>
+                        <TabContent activeTab={this.state.activeTab} onKeyPress={() => { console.log('pressed') }}>
+                            <TabPane tabId="1" >
+                                <Row >
+                                    <Col sm="6">
+                                        <Card body className="tab-card">
+                                            <CardTitle>Choose Theme</CardTitle>
+                                            <CardText>
+                                                Select one of the below themes for your panel.
+    
+    
                                         </CardText>
 
-                                        <div className="themes">
-                                            {
-                                                this.themes.map((theme, key) =>
-                                                    <div onClick={() => this.selectTheme(theme)} key={key} className={`${selectedTheme.class == theme.class ? 'active' : null}  ${theme.class} theme-container`}>
-                                                        <div className="theme-color">
+                                            <div className="themes">
+                                                {
+                                                    this.themes.map((theme, key) =>
+                                                        <div onClick={() => this.selectTheme(theme)} key={key} className={`${selectedTheme.class == theme.class ? 'active' : null}  ${theme.class} theme-container`}>
+                                                            <div className="theme-color">
+                                                            </div>
+                                                            <div className="theme-name">
+                                                                {theme.name}
+                                                            </div>
                                                         </div>
-                                                        <div className="theme-name">
-                                                            {theme.name}
-                                                        </div>
-                                                    </div>
-                                                )
-                                            }
-                                        </div>
-                                    </Card>
-                                </Col>
+                                                    )
+                                                }
+                                            </div>
+                                        </Card>
+                                    </Col>
 
-                                <Col sm="6">
-                                    <Card body className="tab-card">
-                                        <CardTitle>Spacing</CardTitle>
-                                        <CardText>
-                                            Choose the spacing of elements for the panel.
+                                    <Col sm="6">
+                                        <Card body className="tab-card">
+                                            <CardTitle>Spacing</CardTitle>
+                                            <CardText>
+                                                Choose the spacing of elements for the panel.
                                         </CardText>
 
-                                        <div className="spacing">
-                                            {
-                                                this.spacing.map((spacing, key) =>
-                                                    <div onClick={() => this.selectSpacing(spacing)} key={key} className={`${selectedSpacing.class == spacing.class ? 'active' : null}  ${selectedSpacing.class} theme-container`}>
-                                                        <div className="spacing-name">
-                                                            {spacing.name}
+                                            <div className="spacing">
+                                                {
+                                                    this.spacing.map((spacing, key) =>
+                                                        <div onClick={() => this.selectSpacing(spacing)} key={key} className={`${selectedSpacing.class == spacing.class ? 'active' : null}  ${selectedSpacing.class} theme-container`}>
+                                                            <div className="spacing-name">
+                                                                {spacing.name}
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                )
-                                            }
-                                        </div>
+                                                    )
+                                                }
+                                            </div>
 
-                                        {/* Spacing Section */}
-                                        <h3>
-                                        </h3>
-                                        {/* Spacing Ends */}
+                                            {/* Spacing Section */}
+                                            <h3>
+                                            </h3>
+                                            {/* Spacing Ends */}
 
-                                    </Card>
-                                </Col>
+                                        </Card>
+                                    </Col>
 
-                            </Row>
-                        </TabPane>
-                        <TabPane tabId="2" >
-                            <SpotlightTab />
-                        </TabPane>
-                    </TabContent>
+                                </Row>
+                            </TabPane>
+                            <TabPane tabId="2" >
+                                <SpotlightTab />
+                            </TabPane>
+                        </TabContent>
 
-                </ModalBody>
-                <ModalFooter>
-                    <div>
-                        <Button color="secondary" onClick={this.closeModal}>Cancel</Button>
-                        <Button color="primary" onClick={() => this.applyChanges()}>Apply Changes</Button>
-                    </div>
-                </ModalFooter>
-            </div >
-        )
+                    </ModalBody>
+                    <ModalFooter>
+                        <div>
+                            <Button color="secondary" onClick={this.closeModal}>Cancel</Button>
+                            <Button color="primary" onClick={() => this.applyChanges()}>Apply Changes</Button>
+                        </div>
+                    </ModalFooter>
+                </div >
+            )
+        }
     }
-}
 
 
 
