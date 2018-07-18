ccdc613737c12ac043e4b59b3cde2fcf37dbcab7
Akash Kumar Gaur <39940566+Akash-Kumar-Gaur@users.noreply.github.com>
2018-07-18 16:24:11 +0530

    Update modalWrapper.component.js

--- src/Wrappers/Modal-Wrapper/modalWrapper.component.js
+++ src/Wrappers/Modal-Wrapper/modalWrapper.component.js
@@ -67,7 +67,7 @@ export default class ModalWrapper extends Component {
                             console.log(modal, key);
                             this.closeModal(modal, key)
                         }
-                        } className={this.props.className} backdrop={this.state.backdrop} id="modal-container">
+                        } className={this.props.className} backdrop={this.state.backdrop}>
                             {
                                 modal.modalHeader ?
                                     <ModalHeader toggle={() => {
