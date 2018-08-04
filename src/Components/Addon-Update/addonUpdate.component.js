398ba4c330cada3d4592064510a59f868fecdcda
Akash Gaur <akash.gaur@drivezy.com>
2018-08-04 11:07:26 +0530

    merge conflicts

--- src/Components/Addon-Update/addonUpdate.component.js
+++ src/Components/Addon-Update/addonUpdate.component.js
@@ -22,15 +22,9 @@ export default class AddonUpdate extends React.Component {
 
     getAddons = async (rideData) => {
         if (rideData.is_bike == 1) {
-<<<<<<< Updated upstream
             var url = 'accessoryAddon?query=active=1 and bike_only=0';
         } else {
             url = 'accessoryAddon?query=active=1 and bike_only=1';
-=======
-            var url = 'accessoryAddon?query=active=1 and bike_only=1';
-        } else {
-            url = 'accessoryAddon?query=active=1 and bike_only=0';
->>>>>>> Stashed changes
         }
         const result = await Get({ url: url, urlPrefix: 'https://api.justride.in/api/admin/' });
         if (result.success) {
@@ -38,7 +32,6 @@ export default class AddonUpdate extends React.Component {
         }
     }
 
-<<<<<<< Updated upstream
     updateStartAddons = (quantity, addOn) => {
         const { startAddons } = this.state;
         quantity ? startAddons.push(addOn) : startAddons.splice(0,1);
@@ -54,15 +47,6 @@ export default class AddonUpdate extends React.Component {
 
     render() {
         const { addon = {}, rideStatus, addOns = [], startAddons, endAddons} = this.state;
-=======
-    updateValue = (quantity, addon) => {
-        const { addOns } = this.state;
-        console.log(addon)
-    }
-
-    render() {
-        const { addon = {}, rideStatus, addOns = [], startAddons} = this.state;
->>>>>>> Stashed changes
 
         return (
             <div className="addons-wrapper">
@@ -78,16 +62,11 @@ export default class AddonUpdate extends React.Component {
                         {
                             addOns.map((addon, key) => (
                                 (addon.name != 'Complimentary Helmet') ?
-<<<<<<< Updated upstream
                                 <div className="row" key={key}>
-=======
-                                <div className="row">
->>>>>>> Stashed changes
                                     <div className="col-sm-6 no-padding-right">
                                         {addon.name}
                                     </div>
                                     <div className="col-sm-3 no-padding-right no-padding-left">
-<<<<<<< Updated upstream
                                         <div className="btn-group">
                                             <button type="button" className="btn btn-sm btn-danger"  onClick={(e) => this.updateStartAddons(0, addon)}>-</button>
                                             <button type="button" className="btn btn-default"><div>{}</div></button>
@@ -96,16 +75,6 @@ export default class AddonUpdate extends React.Component {
                                     </div>
                                     <div className="col-sm-3">
                                         Rs. {addon.fixed_cost*startAddons.length}
-=======
-                                        <div class="btn-group">
-                                            <button type="button" class="btn btn-sm btn-primary" onClick={(e) => this.updateValue(-1, addon)}>-</button>
-                                            <button type="button" class="btn btn-default"><div>{startAddons.length}</div></button>
-                                            <button type="button" class="btn btn-sm btn-primary" onClick={(e) => this.updateValue(1, addon)}>+</button>
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-3">
-                                        Rs. 0
->>>>>>> Stashed changes
                                     </div>
                                 </div>
                                 :
@@ -126,16 +95,11 @@ export default class AddonUpdate extends React.Component {
                         {
                             addOns.map((addon, key) => (
                                 (addon.name != 'Complimentary Helmet') ?
-<<<<<<< Updated upstream
                                 <div className="row" key={key}>
-=======
-                                <div className="row">
->>>>>>> Stashed changes
                                     <div className="col-sm-6 no-padding-right">
                                         {addon.name}
                                     </div>
                                     <div className="col-sm-3 no-padding-right no-padding-left">
-<<<<<<< Updated upstream
                                         <div className="btn-group">
                                             <button type="button" className="btn btn-sm btn-danger" onClick={(e) => this.updateStartAddons(0, addon)}>-</button>
                                             <button type="button" className="btn btn-default">{startAddons.length}</button>
@@ -144,16 +108,6 @@ export default class AddonUpdate extends React.Component {
                                     </div>
                                     <div className="col-sm-3">
                                         Rs. {addon.fixed_cost*startAddons.length}
-=======
-                                        <div class="btn-group">
-                                            <button type="button" class="btn btn-sm btn-primary">-</button>
-                                            <button type="button" class="btn btn-default">0</button>
-                                            <button type="button" class="btn btn-sm btn-primary">+</button>
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-3">
-                                        Rs. 0
->>>>>>> Stashed changes
                                     </div>
                                 </div>
                                 :
@@ -173,16 +127,11 @@ export default class AddonUpdate extends React.Component {
                             {
                                 addOns.map((addon, key) => (
                                     (addon.name != 'Complimentary Helmet') ?
-<<<<<<< Updated upstream
                                     <div className="row" key={key}>
-=======
-                                    <div className="row">
->>>>>>> Stashed changes
                                         <div className="col-sm-6 no-padding-right">
                                             {addon.name}
                                         </div>
                                         <div className="col-sm-3 no-padding-right no-padding-left">
-<<<<<<< Updated upstream
                                             <div className="btn-group">
                                                 <button type="button" className="btn btn-sm btn-danger" onClick={(e) => this.updateEndAddons(0, addon)}>-</button>
                                                 <button type="button" className="btn btn-default">{endAddons.length}</button>
@@ -191,16 +140,6 @@ export default class AddonUpdate extends React.Component {
                                         </div>
                                         <div className="col-sm-3">
                                             Rs. {addon.fixed_cost*endAddons.length}
-=======
-                                            <div class="btn-group">
-                                                <button type="button" class="btn btn-sm btn-primary">-</button>
-                                                <button type="button" class="btn btn-default">0</button>
-                                                <button type="button" class="btn btn-sm btn-primary">+</button>
-                                            </div>
-                                        </div>
-                                        <div className="col-sm-3">
-                                            Rs. 0
->>>>>>> Stashed changes
                                         </div>
                                     </div>
                                     :
