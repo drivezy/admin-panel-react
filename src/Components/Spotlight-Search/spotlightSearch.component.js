16f0cdaa2993c903b39861f5a8e4143978997c2d
Mangesh Lokhande <mangesh.lokhande@drivezy.com>
2018-08-21 16:02:23 +0530

    Coupon in spotlight done

--- src/Components/Spotlight-Search/spotlightSearch.component.js
+++ src/Components/Spotlight-Search/spotlightSearch.component.js
@@ -96,7 +96,7 @@ export class Spotlight extends Component {
             // else {
             //     obj = 2;     //@ToDp : Carefully write execution for this case
             // }
-            
+
             this.searchVal(obj, searchText);
         }
     }
@@ -105,6 +105,7 @@ export class Spotlight extends Component {
         let result;
         let querySearchList = []
         let url
+        let body
         switch (obj) {
 
             // To search PNR
@@ -136,7 +137,7 @@ export class Spotlight extends Component {
             // To search Payment
             case 3:     //@TODO MAKE A LIST PAGE WHERE WE SHOULD REDIRECT
                 // getSetValue.setValue(searchText);
-                
+
                 break;
 
             // To search vehicle
@@ -149,39 +150,40 @@ export class Spotlight extends Component {
 
             // To search coupon
             case 5:
-                
-                url = "coupon?query=coupon_code=\"" + searchText + "\"";
-                result = await Get({ url: url });
-                
+
+                body = 'coupon_code="' + searchText + '"';
+                result = await Get({ url: "coupon?query=" + body });
+                url = '/campaign/' + result.response[0].campaign_id;
+                Location.navigate({ url: url })
                 break;
 
             // To search Ticket
             case 6:
-                
+
                 // url = "/ticket/" + searchText + "\"";
                 // result = await Get({ url: url });
-               
-                let body = 'ticket_number="'+searchText+'"';
-                result = await Get({ url: "task?query="+body });
+
+                body = 'ticket_number="' + searchText + '"';
+                result = await Get({ url: "task?query=" + body });
                 url = '/ticket/' + result.response[0].id;
-                Location.navigate({url:url})
+                Location.navigate({ url: url })
                 break;
 
             // To search Vendor
             case 7:     //@TODO MAKE A LIST PAGE WHERE WE SHOULD REDIRECT
-                
+
                 break;
 
             // To search Ticket
             case 8:
-                
+
                 url = "expenseVoucher?query=invoice=\"" + searchText + "\"";
                 result = await Get({ url: url });
-                
+
                 break;
 
             case 9:     //@TODO MAKE A LIST PAGE WHERE WE SHOULD REDIRECT
-                
+
                 break;
 
             default:
@@ -205,7 +207,7 @@ export class Spotlight extends Component {
                 this.searchInput.current.blur();
             }
             else if (event.which == 13) {
-                
+
                 this.redirectTo(this.state.querySearchList[this.state.querySearchList.length - 1]);
 
                 this.advancedSearch();
