af4b096de821fcf211a7a1bf9c453579b8fb130b
Akash Gaur <akash.gaur@drivezy.com>
2018-08-28 16:09:01 +0530

    Booking Source Display

--- src/Scenes/Booking-Detail/bookingDetail.scene.js
+++ src/Scenes/Booking-Detail/bookingDetail.scene.js
@@ -86,7 +86,7 @@ export default class BookingDetail extends Component {
             <div className="booking">
                 <div className="booking-header">
                     <div className="booking-info">
-                        Booking Deatils View | {bookingDetail.token} | {bookingDate}
+                        Booking Deatils View | {bookingDetail.token} | {bookingDate} | {bookingDetail.application ? bookingDetail.application.name : null}
                     </div>
                     <div className="header-actions">
                         <button className="refresh-button btn btn-sm" onClick={(e) => { this.refreshPage(e) }}>
